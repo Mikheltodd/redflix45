618c7446a4923d34e8dd327e41f5e362ad50a4ac
eslunap19 <estefany.luna@gmail.com>
2020-11-29 14:37:43 -0500

    Creacion estructura proyecto java
    Hola recuerden que mientras estemos haciendo pruebas locales, cada quien debe colocar sus credenciales en el archivo "application.properties"

---
+++ redflix45 project/InitializrSpringbootProject/src/test/java/co/g2m2e1/redflix45/Redflix45ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.g2m2e1.redflix45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redflix45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
