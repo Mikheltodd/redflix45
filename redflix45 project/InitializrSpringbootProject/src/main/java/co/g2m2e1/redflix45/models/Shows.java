1f416206dde6da3a5ea3695f39ce50624dae0aec
eslunap19 <estefany.luna@gmail.com>
2020-12-01 09:11:41 -0500

    Esqueleto creado

---
+++ redflix45 project/InitializrSpringbootProject/src/main/java/co/g2m2e1/redflix45/models/Shows.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.g2m2e1.redflix45.models;
+
+/**
+ *
+ * @author ESLUNAP
+ */
+public class Shows {
+    
+}
