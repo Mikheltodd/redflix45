3b26020716ddbaeb8eb8ac5c221fa317791440bb
eslunap19 <estefany.luna@gmail.com>
2020-12-01 09:57:30 -0500

    agregado component scan
    se agrego component Scan  a clase Redflix45Application.java

--- redflix45 project/InitializrSpringbootProject/src/main/java/co/g2m2e1/redflix45/Redflix45Application.java
+++ redflix45 project/InitializrSpringbootProject/src/main/java/co/g2m2e1/redflix45/Redflix45Application.java
@@ -3,8 +3,10 @@ package co.g2m2e1.redflix45;
 import co.g2m2e1.redflix45.views.MainFrame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("co.com.g2m2e1.redflix45")
 public class Redflix45Application {
 
 	public static void main(String[] args) {
